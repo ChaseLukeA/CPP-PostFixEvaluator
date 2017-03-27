73d5102b2161eca5e1016e93b40b09b58eea4008
ChaseLukeA <chase.luke.a@gmail.com>
2017-03-27 17:02:41 -0500

    Update wait for any key at end

--- P3_PostFixEvaluator/P3_PostFixEvaluator.cpp
+++ P3_PostFixEvaluator/P3_PostFixEvaluator.cpp
@@ -3,15 +3,32 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <thread>
+#include <atomic>
 #include <string>
 #include "P3_PostFixEvaluator.h"
 #include "PostFixEvaluator.h"
 
+#ifdef _WIN32 || _WIN64
+	#include <Windows.h>
+	void sleep(unsigned milliseconds) { Sleep(milliseconds); }
+	void pause() { system("pause"); }
+#else
+	#include <unistd.h>
+	void sleep(unsigned milliseconds) { usleep(milliseconds * 1000); }
+	void pause() { system("read -n1"); }
+#endif
+
 using namespace std;
+
+
+atomic_bool anyKeyPressed(false);
+void waitForAnyKey();
+void doUntilAnyKeyPressed();
+
 
 int main()
 {
-	int pause;
 	string more = "y";
 
 	PostFixEvaluator pfe;
@@ -27,8 +44,26 @@ int main()
 		cout << endl;
 	}
 
-	cout << "Thank you for using the PostFix Evaluator!" << endl << endl << "Press any key to exit";
-	getchar();
+	cout << "Thank you for using the PostFix Evaluator!" << endl << endl;
+
+	waitForAnyKey();
+
     return 0;
 }
 
+void waitForAnyKey()
+{
+	thread waitForKey = thread(doUntilAnyKeyPressed);
+	pause();
+	anyKeyPressed = true;
+	waitForKey.join();
+}
+
+void doUntilAnyKeyPressed()
+{
+	while (!anyKeyPressed)
+	{
+		sleep(1000);
+		cout << ". ";
+	}
+}
